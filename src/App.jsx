a5d1cae45adbfc98ec144ca2ed78d95c58e5560e
Stanley12590 <masanyiwastanley378@gmail.com>
2025-11-14 00:10:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import LiveChannels from './pages/LiveChannels'
 import Categories from './pages/Categories'
 import Banners from './pages/Banners'
 import Layout from './components/Layout'
+import './index.css'
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useAuth()
@@ -23,50 +24,67 @@ function ProtectedRoute({ children }) {
   return user ? children : <Navigate to="/login" />
 }
 
+function AppRoutes() {
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-red-600"></div>
+      </div>
+    )
+  }
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Layout>
+            <Dashboard />
+          </Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/movies" element={
+        <ProtectedRoute>
+          <Layout>
+            <Movies />
+          </Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/channels" element={
+        <ProtectedRoute>
+          <Layout>
+            <LiveChannels />
+          </Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/categories" element={
+        <ProtectedRoute>
+          <Layout>
+            <Categories />
+          </Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/banners" element={
+        <ProtectedRoute>
+          <Layout>
+            <Banners />
+          </Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+}
+
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={
-            <ProtectedRoute>
-              <Layout>
-                <Dashboard />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/movies" element={
-            <ProtectedRoute>
-              <Layout>
-                <Movies />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/channels" element={
-            <ProtectedRoute>
-              <Layout>
-                <LiveChannels />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/categories" element={
-            <ProtectedRoute>
-              <Layout>
-                <Categories />
-              </Layout>
-            </ProtectedRoute>
-          } />
-          <Route path="/banners" element={
-            <ProtectedRoute>
-              <Layout>
-                <Banners />
-              </Layout>
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </AuthProvider>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </AuthProvider>
   )
 }
 
